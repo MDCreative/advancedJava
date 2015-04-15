69759acfa1b720fcedea6374b900ec1eeb301f2c
LiamAChapman <eeu239@bangor.ac.uk>
2015-04-15 12:09:32 +0100

    Update CreateTables.sql

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `user` (
   `user_id` INT NOT NULL,
-  `username` VARCHAR(255) ,
-  `password` VARCHAR(255),
+  `username` VARCHAR(50) ,
+  `password` VARCHAR(20),
   `type` INT NOT NULL,
   PRIMARY KEY `user_id` (`user_Id`)
 );# MySQL returned an empty result set (i.e. zero rows).
@@ -16,13 +16,13 @@ CREATE TABLE IF NOT EXISTS `test_history` (
 
 
 CREATE TABLE IF NOT EXISTS `word`(
-    `word` VARCHAR(255) NOT NULL,
-    `category` VARCHAR(255) NOT NULL,
-    `translation` VARCHAR(255) NOT NULL,
+    `word` VARCHAR(30) NOT NULL,
+    `category` VARCHAR(20) NOT NULL,
+    `translation` VARCHAR(50) NOT NULL,
     `gender`INT NOT NULL,
     `total_tests` INT NOT NULL,
     `correct_ans` INT NOT NULL,
-    `alternate_sex` VARCHAR(255) NOT NULL,
+    `alternate_sex` INT NOT NULL,
     `incorrect_ans` INT NOT NULL,
     PRIMARY KEY `word` (`word`)
 );# MySQL returned an empty result set (i.e. zero rows).
